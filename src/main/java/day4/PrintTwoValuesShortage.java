0690c5a6a4bdf119636c608e4fbc5c7e8effc568
yudaeon <daeon03050@gmail.com>
2023-04-20 11:27:45 +0900

    PrintfEx first

---
+++ src/main/java/day4/PrintTwoValuesShortage.java
@@ -0,0 +1,8 @@
+package day4;
+
+public class PrintTwoValuesShortage {
+    public static void main(String[] args) {
+        System.out.printf("%s님 안녕하세요. 그런데 제가 하고 싶은 말은 %s입니다.",
+                "유다언");
+    }
+}
