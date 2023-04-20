ea096ee0ab70f4f427fcab614a3c93cf6a65b239
yudaeon <daeon03050@gmail.com>
2023-04-20 16:28:07 +0900

    feat: codeup1015

---
+++ src/main/java/codeup/codeup1015.java
@@ -0,0 +1,11 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup1015 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float f = scanner.nextFloat();
+        System.out.println(String.format("%.2f", f));
+    }
+}
