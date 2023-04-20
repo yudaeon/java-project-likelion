26404897c94d5f6b3ff601ef26fa3d9b1e25f117
yudaeon <daeon03050@gmail.com>
2023-04-20 13:37:28 +0900

    소수점 자릿수 구하기

---
+++ src/main/java/day4/PrintfFloatDec.java
@@ -0,0 +1,8 @@
+package day4;
+public class PrintfFloatDec {
+    public static void main(String[] args) {
+        System.out.printf("%f\n", 0.5);
+        System.out.printf("%.1f\n", 1.44);
+        System.out.printf("%.2f\n", 3.78987);
+    }
+}
