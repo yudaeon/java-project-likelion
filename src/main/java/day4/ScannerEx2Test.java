9cfe5b6178579eadea4a668d0026726d7b86262e
yudaeon <daeon03050@gmail.com>
2023-04-20 10:47:16 +0900

    day4 ScannerStrEx

---
+++ src/main/java/day4/ScannerEx2Test.java
@@ -0,0 +1,8 @@
+package day4;
+
+public class ScannerEx2Test {
+    public static void main(String[] args) {
+        ScannerEx2 scannerEx2 = new ScannerEx2();
+        scannerEx2.inputTwoNumberAndPlus();
+    }
+}
