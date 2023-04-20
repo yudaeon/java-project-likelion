ad4700743db73a5f555fcf5573e72771e3f88fc7
yudaeon <daeon03050@gmail.com>
2023-04-20 15:41:31 +0900

    feat: codeup1014

--- src/main/java/codeup/codeup1014.java
+++ src/main/java/codeup/codeup1014.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class codeup1014 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        char x = scanner.next().charAt(0);
-        char y = scanner.next().charAt(0);
-        System.out.println(y+" "+x);
+        Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        String y = sc.next();
+        System.out.printf("%s %s", y, x);
     }
 }
