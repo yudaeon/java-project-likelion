4d619536d034047411fbc619b7c587a463f09115
yudaeon <daeon03050@gmail.com>
2023-04-20 09:41:42 +0900

    day4 ScannerStrEx

---
+++ src/main/java/day4/ScannerStrExTest.java
@@ -0,0 +1,10 @@
+package day4;
+
+import java.util.Scanner;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx scannerStrEx = new ScannerStrEx();
+        scannerStrEx.inputStringsAndPlus();
+    }
+}
