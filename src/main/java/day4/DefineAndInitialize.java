edcb01350a314b1cb20bd545000e202ce1e8ce0d
yudaeon <daeon03050@gmail.com>
2023-04-20 15:28:18 +0900

    feat: DefineAndInitialize

---
+++ src/main/java/day4/DefineAndInitialize.java
@@ -0,0 +1,15 @@
+package day4;
+
+import java.util.Scanner;
+
+public class DefineAndInitialize {
+    public static void main(String[] args) {
+        int ival = 1; //변수선언
+        PrintHello printHello = new PrintHello();
+        Scanner scanner = new Scanner(System.in);
+
+        Scanner sc; //스캐너 타입에 sc선언
+        sc = new Scanner(System.in);
+
+    }
+}
