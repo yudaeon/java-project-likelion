27ae75e9a96cf5a3aea14429258b0e98084dceb1
yudaeon <daeon03050@gmail.com>
2023-04-20 13:37:42 +0900

    codeup1010

---
+++ src/main/java/codeup/codeup1010.java
@@ -0,0 +1,11 @@
+package codeup;
+
+import java.util.Scanner;
+public class codeup1010 {
+    public static void main(String[] args) {
+        int n ;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        System.out.println(n);
+    }
+}
