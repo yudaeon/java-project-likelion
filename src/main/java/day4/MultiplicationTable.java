f2048bb27f1d06ba229c53d1df0d17aea8a0cc45
yudaeon <daeon03050@gmail.com>
2023-04-20 13:16:26 +0900

    구구단 출력 printf

--- src/main/java/day4/MultiplicationTable.java
+++ src/main/java/day4/MultiplicationTable.java
@@ -6,7 +6,9 @@ public class MultiplicationTable {
     //%s로도 가능합니다. 하지만 1+2 -> x 문자열이기때문에 실행 x
     //%d, 1+2 -> 0 숫자이기때문에 실행 가능
     public static void main(String[] args) {
-        System.out.println("2" + "*" + "1" + "=" + "2");
-        System.out.printf("%d * %d = %d", 2, 2, 4);
+        System.out.printf("%d * %d = %d\n", 2, 2, 4);
+        System.out.printf("%d * %d = %d\n", 2, 3, 6);
+        System.out.printf("%d * %d = %d\n", 2, 4, 8);
+        System.out.printf("%d * %d = %d\n", 2, 5, 10);
     }
 }
