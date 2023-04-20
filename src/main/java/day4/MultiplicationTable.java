a7393b9d9b9ff6f5cce05a69cf7139b2bbd74e2c
yudaeon <daeon03050@gmail.com>
2023-04-20 13:17:09 +0900

    구구단 출력 printf

--- src/main/java/day4/MultiplicationTable.java
+++ src/main/java/day4/MultiplicationTable.java
@@ -10,5 +10,9 @@ public class MultiplicationTable {
         System.out.printf("%d * %d = %d\n", 2, 3, 6);
         System.out.printf("%d * %d = %d\n", 2, 4, 8);
         System.out.printf("%d * %d = %d\n", 2, 5, 10);
+        System.out.printf("%d * %d = %d\n", 2, 6, 12);
+        System.out.printf("%d * %d = %d\n", 2, 7, 14);
+        System.out.printf("%d * %d = %d\n", 2, 8, 16);
+        System.out.printf("%d * %d = %d\n", 2, 9, 18);
     }
 }
