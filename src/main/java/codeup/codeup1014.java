d0e83de56931dcd20b440a8354b6ef59a044f7d5
yudaeon <daeon03050@gmail.com>
2023-04-20 15:36:56 +0900

    feat: codeup1014

--- src/main/java/codeup/codeup1014.java
+++ src/main/java/codeup/codeup1014.java
@@ -1,4 +1,12 @@
 package codeup;
 
+import java.util.Scanner;
+
 public class codeup1014 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char x = scanner.next().charAt(0);
+        char y = scanner.next().charAt(0);
+        System.out.println(y+" "+x);
+    }
 }
