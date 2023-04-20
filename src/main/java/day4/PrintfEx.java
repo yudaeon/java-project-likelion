156543cb08b3bbb6f66ae08848a4f2ee5e8747d7
yudaeon <daeon03050@gmail.com>
2023-04-20 11:19:13 +0900

    PrintfEx first

---
+++ src/main/java/day4/PrintfEx.java
@@ -0,0 +1,16 @@
+package day4;
+
+public class PrintfEx {
+    public void print(){
+        System.out.printf("%s", "Hello\n");
+        System.out.printf("bye");
+    }
+    public void waterCannon(){
+
+    }
+
+    public static void main(String[] args) {
+        PrintfEx printfEx = new PrintfEx();
+        printfEx.print();  //printEx야 물대포를 쏴줘
+    }
+}
