8ccc4a040067bf5d73ff9434ae797077ac90ef5f
yudaeon <daeon03050@gmail.com>
2023-04-21 11:04:30 +0900

    feat: codeup1018

---
+++ src/main/java/codeup/codeup1018.java
@@ -0,0 +1,10 @@
+package codeup;
+
+import java.util.Scanner;
+public class codeup1018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        System.out.printf("%s", x);
+    }
+}
