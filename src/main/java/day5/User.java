2f48f5851b1be1749481a56fc3eedf8dec98d0e4
yudaeon <daeon03050@gmail.com>
2023-04-21 15:14:32 +0900

    feat:User object

---
+++ src/main/java/day5/User.java
@@ -0,0 +1,9 @@
+package day5;
+
+public class User {
+    // 이름, 전화번호, 나이
+
+    String name;
+    String phoneNumber;
+    int age;
+}
---
+++ src/main/java/day5/UserTest.java
@@ -0,0 +1,16 @@
+package day5;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User user = new User();
+        user.name = "유다언";
+        user.phoneNumber = "010-1111-2222";
+        user.age = 37;
+
+        User daeon = new User();
+        daeon.name = "다언";
+        daeon.phoneNumber = "010-0000-2222";
+        daeon.age = 26;
+
+    }
+}
