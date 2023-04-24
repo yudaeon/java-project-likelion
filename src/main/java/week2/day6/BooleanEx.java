27c096f2fd8f485d65b03af59acc4539db14eb16
yudaeon <daeon03050@gmail.com>
2023-04-24 14:35:54 +0900

    feat : BooleanEx. BooleanTypeResult 실습

---
+++ src/main/java/week2/day6/BooleanEx.java
@@ -0,0 +1,14 @@
+package week2.day6;
+
+public class BooleanEx {
+    public static void main(String[] args) {
+        boolean flag1 = false;
+        boolean flag2 = true;
+
+        boolean isPaymentSuccess = true;
+        boolean isBalanceSufficient = false;
+
+        System.out.println(isPaymentSuccess);
+        System.out.println(isBalanceSufficient);
+    }
+}
