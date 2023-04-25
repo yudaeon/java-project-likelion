e8ef7be9c9aa5c54b526045226d2f6089a2b77d3
yudaeon <daeon03050@gmail.com>
2023-04-25 14:30:03 +0900

    feat : Arrays commit

---
+++ src/main/java/week2/day7/ArraysDefineAndPrint4.java
@@ -0,0 +1,10 @@
+package week2.day7;
+
+public class ArraysDefineAndPrint4 {
+    public static void main(String[] args) {
+        //new 연산자로 {}중괄호 안에 배열을 넣을 요소정하기
+        int[] iArr = new int[]{1,2,3,8};
+        System.out.printf("%d %d %d %d", iArr[0],iArr[1],iArr[2],iArr[3]);
+        //1 2 3 8 출력
+    }
+}
---
+++ src/main/java/week2/day7/DefAndInitArr.java
@@ -0,0 +1,10 @@
+package week2.day7;
+
+public class DefAndInitArr {
+    public static void main(String[] args) {
+        int[] iArr = new int[5]; //배열의 개수를 정하기
+        System.out.println(iArr[0]); //[I@448139f0 이런식의 해시 코드가 출력됨
+        System.out.println(iArr[1]); //[I@448139f0 이런식의 해시 코드가 출력됨
+        System.out.println(iArr[2]); //[I@448139f0 이런식의 해시 코드가 출력됨
+    }
+}
