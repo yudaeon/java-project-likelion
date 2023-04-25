0c75d0116998feff430d393aded5c56320e40cdb
yudaeon <daeon03050@gmail.com>
2023-04-25 14:50:40 +0900

    feat : ReferenceTypeArray & DefAndInitArr

---
+++ src/main/java/week2/day7/ReferenceTypeArray.java
@@ -0,0 +1,13 @@
+package week2.day7;
+
+import java.util.Arrays;
+
+public class ReferenceTypeArray {
+    public static void main(String[] args) {
+        float[] fArr = new float[3];
+        String[] sArr = new String[10];
+        System.out.println(Arrays.toString(fArr));
+        System.out.println(Arrays.toString(sArr));
+        //[0.0, 0.0, 0.0] / [null, null, null, null, null, null, null, null, null, null]출력
+    }
+}
