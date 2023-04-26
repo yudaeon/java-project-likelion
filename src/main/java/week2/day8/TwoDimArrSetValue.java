86518e009be8431e12dca3ef5ce7c72027eaa7a3
yudaeon <daeon03050@gmail.com>
2023-04-26 11:37:13 +0900

    feat : TwoDimArraysetValue, TwoDimArrEx

---
+++ src/main/java/week2/day8/TwoDimArrSetValue.java
@@ -0,0 +1,18 @@
+package week2.day8;
+
+import java.util.Arrays;
+
+public class TwoDimArrSetValue {
+    public static void main(String[] args) {
+        // 메소드로 Refactoring을 할 수 있음
+
+        int[][] arr = new int[3][3];
+
+        arr[0][0] = 1;
+        arr[1][0] = 2;
+        arr[0][2] = 3;
+        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[1]));
+        System.out.println(Arrays.toString(arr[2]));
+    }
+}
