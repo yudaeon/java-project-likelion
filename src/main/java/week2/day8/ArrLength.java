d5265d2f8e9254909f483c87a28d24aa017687a0
yudaeon <daeon03050@gmail.com>
2023-04-26 14:18:07 +0900

    feat : codeup 1043

---
+++ src/main/java/week2/day8/ArrLength.java
@@ -0,0 +1,10 @@
+package week2.day8;
+
+public class ArrLength {
+    //배열의 길이
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        System.out.println(arr.length);
+    }
+}
