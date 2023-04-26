3e7580624a0767ef46cbb83a2d17152c113067fb
yudaeon <daeon03050@gmail.com>
2023-04-26 11:18:56 +0900

    feat : TwoDimArrayArrays

---
+++ src/main/java/week2/day8/ArrayDefineAndAssign.java
@@ -0,0 +1,16 @@
+package week2.day8;
+
+import java.util.Arrays;
+
+public class ArrayDefineAndAssign {
+    public static void main(String[] args) {
+        int[][] arr2 = {
+                {10, 20, 30},
+                {40, 50, 60},
+                {70, 80, 90}
+        };
+        System.out.println(Arrays.toString(arr2[0]));
+        System.out.println(Arrays.toString(arr2[1]));
+        System.out.println(Arrays.toString(arr2[2]));
+    }
+}
