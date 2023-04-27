953bfbe352a6a1244293018b174b34202fda2624
yudaeon <daeon03050@gmail.com>
2023-04-27 16:25:06 +0900

    feat : ComparisonArray, BitOperatorEx

---
+++ src/main/java/week2/day9/BitOperatorEx.java
@@ -0,0 +1,15 @@
+package week2.day9;
+
+public class BitOperatorEx {
+    public static void main(String[] args) {
+        int val1 = 1;
+
+        System.out.println(val1 << 1);
+        System.out.println(val1 << 2);
+        System.out.println(val1 << 3);
+
+        int val2 = 32;
+        System.out.println(val2 >> 1);
+        System.out.println(val2 >> 2);
+    }
+}
---
+++ src/main/java/week2/day9/ComparisonArray.java
@@ -0,0 +1,20 @@
+package week2.day9;
+
+import java.util.Arrays;
+
+public class ComparisonArray {
+    //버블정렬
+    public static void main(String[] args) {
+        int[] arr = {2,1,7,9};
+        System.out.printf("%d < %d --> %b\n", arr[0], arr[1], arr[0] > arr[1]); //2,1중에 1이 더작니? true
+        int temp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = temp; //  1과 2의 순서를 바꾸기
+        System.out.println(Arrays.toString(arr)); // 1, 2, 7, 9 출력
+        System.out.printf("%d < %d --> %b\n", arr[1], arr[2], arr[1] > arr[2]);
+        System.out.println(Arrays.toString(arr)); // 1, 2, 7, 9 출력
+        System.out.printf("%d < %d --> %b\n", arr[2], arr[3], arr[2] > arr[3]);
+        System.out.println(Arrays.toString(arr)); // 1, 2, 7, 9 출력
+
+    }
+}
