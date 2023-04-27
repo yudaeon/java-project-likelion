d3431c1caa62fe57c9f795dc460c8ffc0c11cd0c
yudaeon <daeon03050@gmail.com>
2023-04-27 09:12:39 +0900

    feat : Accumulate687, Accumulate687While

--- src/main/java/week2/day8/Acumulate687.java
+++ src/main/java/week2/day8/Acumulate687.java
@@ -2,18 +2,30 @@ package week2.day8;
 
 public class Acumulate687 {
     public static void main(String[] args) {
+        //변수,  누적, 연산자, 나머지, 몫
         int num = 687;
         int answer = 0;
 
-        answer = answer + num % 10;
-        System.out.printf("answer:%d\n", answer);
-
+        //나머지를 먼저 구하고
+        answer = answer + (num % 10);
+        //그 뒤에 몫을 구해야합니다.
         num = num / 10;
-        answer = answer + num % 10;
-        System.out.printf("answer:%d\n", answer);
+        System.out.printf("num%d, answer:%d\n", num, answer);
+        //68-->10으로 나눈 나머지 = 8
 
+        //나머지를 먼저 구하고
+        answer = answer + (num % 10);
+        //그 뒤에 몫을 구해야합니다.
         num = num / 10;
-        answer = answer + num % 10;
-        System.out.printf("answer:%d\n", answer);
+        System.out.printf("num%d, answer:%d\n", num, answer);
+        //68-->10으로 나눈 나머지 = 8
+
+        //나머지를 먼저 구하고
+        answer = answer + (num % 10);
+        //그 뒤에 몫을 구해야합니다.
+        num = num / 10;
+        System.out.printf("num%d, answer:%d\n", num, answer);
+        //68-->10으로 나눈 나머지 = 8
+
     }
 }
---
+++ src/main/java/week2/day9/Accumulate687While.java
@@ -0,0 +1,14 @@
+package week2.day9;
+
+public class Accumulate687While {
+    public static void main(String[] args) {
+        int num = 687;
+        int answer = 0;
+
+        while (num > 0) {
+            answer = answer + (num % 10);
+            num = num / 10;
+        }
+            System.out.println(answer);
+    }
+}
