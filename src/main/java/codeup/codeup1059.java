376484abf61edb43966d25c2f63f15c7273ddd8a
yudaeon <daeon03050@gmail.com>
2023-04-27 14:50:28 +0900

    feat : codeup1059-1062

---
+++ src/main/java/codeup/codeup1059.java
@@ -0,0 +1,11 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup1059 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        System.out.println(~val1);
+    }
+}
---
+++ src/main/java/codeup/codeup1061.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup1061 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+        System.out.println(val1 | val2); // 비트연산자 or
+    }
+}
