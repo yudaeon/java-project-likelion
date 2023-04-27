bcd27ed22c517ff0ff2a633d95ad216b4c6a4cb7
yudaeon <daeon03050@gmail.com>
2023-04-27 14:50:20 +0900

    feat : codeup1059-1062

---
+++ src/main/java/codeup/codeup1062.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup1062 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+        System.out.println(val1 ^ val2); //XOR비트연산자
+    }
+}
---
+++ src/main/java/week2/day9/BitOperatorEx2.java
@@ -0,0 +1,10 @@
+package week2.day9;
+
+public class BitOperatorEx2 {
+    public static void main(String[] args) {
+        int val1 = 1;
+         System.out.println(~val1); //-2
+        System.out.println(Integer.toBinaryString(val1)); // 1
+        System.out.println(Integer.toBinaryString(~val1)); //111111111110
+    }
+}
