bc71a0830f87b76b77828611134157b4b017aff5
yudaeon <daeon03050@gmail.com>
2023-04-27 13:23:43 +0900

    feat : codeup1053-1055

---
+++ src/main/java/codeup/codeup1053.java
@@ -0,0 +1,15 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup1053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+
+        if (val1 == 0 )
+            System.out.printf("1");
+        else
+            System.out.printf("0");
+    }
+}
---
+++ src/main/java/codeup/codeup1055.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup1055 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt();
+        int val2 = sc.nextInt();
+        System.out.println(( val1 == 1 || val2 == 1) ? 1 : 0); //OR연산자
+    }
+}
