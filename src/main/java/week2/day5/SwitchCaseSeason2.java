b882e78375af6b940c707bef8e975d54eea03ecf
yudaeon <daeon03050@gmail.com>
2023-04-28 11:47:23 +0900

    feat : SwitchCaseSeason2

---
+++ src/main/java/week2/day5/SwitchCaseSeason2.java
@@ -0,0 +1,23 @@
+package week2.day5;
+
+public class SwitchCaseSeason2 {
+    public static void main(String[] args) {
+        int month = 6;
+        switch (month){
+            case 12, 1, 2:
+                System.out.println("겨울");
+                break;
+            case 3, 4, 5:
+                System.out.println("봄");
+                break;
+            case 6, 7, 8:
+                System.out.println("여름");
+                break;
+            case 9, 10, 11:
+                System.out.println("가을");
+                break;
+            default:
+                System.out.println("해당하는 계절이 없습니다");
+        }
+    }
+}
