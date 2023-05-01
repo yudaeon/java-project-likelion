782bcbad71f3cbc6747f96ea1605720afc3511b2
yudaeon <daeon03050@gmail.com>
2023-05-01 13:13:20 +0900

    feat : ForLoopForwardStep

---
+++ src/main/java/week3/day1/ForLoopForwardStep.java
@@ -0,0 +1,10 @@
+package week3.day1;
+
+public class ForLoopForwardStep {
+    public static void main(String[] args) {
+        //2씩 증가하기
+        for (int i = 0; i < 10; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
