e02211c331b1d37c5a9ceac8ff2bf4f19d7756b4
yudaeon <daeon03050@gmail.com>
2023-05-01 11:21:20 +0900

    feat : ForLoopForward1To10,ForLoopReverse

---
+++ src/main/java/week3/day1/ForLoopReverse.java
@@ -0,0 +1,10 @@
+package week3.day1;
+
+public class ForLoopReverse {
+    public static void main(String[] args) {
+        // 5부터 역순으로 출력 54321
+        for (int i = 5; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
