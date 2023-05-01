bd43e15f180f61a1db99597f6ee9e3e74013e54c
yudaeon <daeon03050@gmail.com>
2023-05-01 09:14:02 +0900

    feat : codeup1103-1106

---
+++ src/main/java/codeup/codeup1106.java
@@ -0,0 +1,9 @@
+package codeup;
+
+public class codeup1106 {
+    public static void main(String[] args) {
+    int min = Integer.MIN_VALUE;
+    int max = Integer.MAX_VALUE;
+    System.out.printf("%d %d\n", min, max);
+    }
+}
