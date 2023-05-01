1bd7b3ed05caef33fdf1cf2eaede602536f721e9
yudaeon <daeon03050@gmail.com>
2023-05-01 14:26:22 +0900

    feat : MultiplicationTableStep2

--- src/main/java/week3/day1/MultiplicationTableStep2.java
+++ src/main/java/week3/day1/MultiplicationTableStep2.java
@@ -6,5 +6,13 @@ public class MultiplicationTableStep2 {
         for (int i = 1; i <= 9; i++) {
             System.out.printf("2 * %d = %d\n", i, 2 * i);
         }
+        System.out.println("---------------");
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("3 * %d = %d\n", i, 3 * i);
+        }
+        System.out.println("-------------");
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("4 * %d = %d\n", i, 4 * i);
+        }
     }
 }
