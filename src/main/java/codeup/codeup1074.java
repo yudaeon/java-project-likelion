3faf55cac9f7b7b6cb783654e83c686dbccffabe
yudaeon <daeon03050@gmail.com>
2023-05-01 11:50:20 +0900

    feat : codeup1074-1075

---
+++ src/main/java/codeup/codeup1074.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup1074 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = sc.nextInt(); i >= 1 ; --i) {
+            System.out.println(i);
+        }
+        }
+    }
---
+++ src/main/java/codeup/codeup1075.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup1075 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = sc.nextInt()-1; i >= 0 ; --i) {
+            System.out.println(i);
+        }
+    }
+}
