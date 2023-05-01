50cce1829c6434652a8bd6595dd4e1c94354c8da
yudaeon <daeon03050@gmail.com>
2023-05-01 10:31:28 +0900

    feat : codeup1071

---
+++ src/main/java/codeup/codeup1071.java
@@ -0,0 +1,15 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup1071 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0;; i++) {
+            int val1 = sc.nextInt();
+                if (val1==0){
+                    break;}
+                System.out.println(val1);
+            }
+    }
+}
