749af0d0986c45b3226d4d9c912f92adab4e87d3
yudaeon <daeon03050@gmail.com>
2023-05-01 15:32:20 +0900

    feat : codeup1081-1082

---
+++ src/main/java/codeup/codeup1081.java
@@ -0,0 +1,17 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup1081 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m  = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <=m; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
+    }
+}
---
+++ src/main/java/codeup/codeup1082.java
@@ -0,0 +1,15 @@
+package codeup;
+
+import javax.swing.plaf.SeparatorUI;
+import javax.swing.plaf.synth.SynthUI;
+import java.util.Scanner;
+
+public class codeup1082 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val1 = sc.nextInt(16); //16진수
+        for (int i = 1; i < 16 ; i++) {
+            System.out.printf("%X*%X=%X \n", val1, i, val1 * i); //구구단
+        }
+    }
+}
