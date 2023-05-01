e025132f6ab54f35ee8c280deed60d6fb5d373b6
yudaeon <daeon03050@gmail.com>
2023-05-01 14:18:01 +0900

    feat : MultiplicationTable

---
+++ src/main/java/week3/day1/MultiplicationTable.java
@@ -0,0 +1,10 @@
+package week3.day1;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        //구구단 출력
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("%d\n", 2 * i);
+        }
+    }
+}
