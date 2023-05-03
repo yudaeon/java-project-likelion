6fe60b2af386c3c9ab5a619aa0d7deadf4870dae
yudaeon <daeon03050@gmail.com>
2023-05-03 11:46:52 +0900

    feat : IsPrime 소수인지 판별하기

---
+++ src/main/java/week3/day3/IsPrime.java
@@ -0,0 +1,13 @@
+package week3.day3;
+
+public class IsPrime {
+    public static void main(String[] args) {
+        int num = 7;
+        int factors = 0; //약수의 개수
+        for (int i = 2; i < num; i++) {  //7의 경우는 1~6까지
+            if (num % i == 0) factors++; //factors =1
+        }
+        System.out.printf("factors:%d\n", factors);
+        System.out.println(factors == 0 ? num + "은 소수입니다." : num + "은 소수가 아닙니다.");
+    }
+}
