3286b49cb5a1ae8c10d03b46dde3401012cc741c
yudaeon <daeon03050@gmail.com>
2023-05-08 10:22:59 +0900

    feat : codeup1256 별출력하기

---
+++ src/main/java/codeup/codeup1256.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class codeup1256 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int star = sc.nextInt();
+            for (int j = 0; j < star;  j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
