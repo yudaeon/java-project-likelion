b57accbbf626067e7c000b6d61b58f521bd0c943
yudaeon <daeon03050@gmail.com>
2023-05-09 15:29:29 +0900

    feat : DiEx&DiExTest

---
+++ src/main/java/week4/day2/DiEx.java
@@ -0,0 +1,15 @@
+package week4.day2;
+
+public class DiEx {
+    private ShapeDrawer shapeDrawer;
+
+    public DiEx(){
+    }
+    public DiEx(ShapeDrawer sd){
+        this.shapeDrawer = sd;
+    }
+
+    public void doSth (){
+        shapeDrawer.printShape(5);
+    }
+}
---
+++ src/test/java/week4/day2/DiExTest.java
@@ -0,0 +1,11 @@
+package week4.day2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiExTest {
+    public static void main(String[] args) {
+        DiEx diEx = new DiEx();
+        diEx.doSth();
+    }
+
+}
