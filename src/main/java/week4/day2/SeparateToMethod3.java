254ddc15ca7833192cae01757523b13736f6cedd
yudaeon <daeon03050@gmail.com>
2023-05-09 11:43:06 +0900

    feat : printArr()리팩토링

---
+++ src/main/java/week4/day2/SeparateToMethod3.java
@@ -0,0 +1,21 @@
+package week4.day2;
+
+import java.util.Arrays;
+
+public class SeparateToMethod3 {
+
+    public static void printArr(int[][] arr){
+        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[1]));
+        System.out.println(Arrays.toString(arr[2]));
+
+    }
+    public static void main(String[] args) {
+        int[][] arr = {
+                {10, 20, 30},
+                {40, 50, 60},
+                {70, 80, 90}
+        };
+        printArr(arr);
+    }
+}
