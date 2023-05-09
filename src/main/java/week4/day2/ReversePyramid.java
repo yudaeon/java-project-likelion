53bb3cd8a134263c950d2a39956696fa89d35ad2
yudaeon <daeon03050@gmail.com>
2023-05-09 10:29:48 +0900

    feat : ReversePyramid 역피라미드 만들기

---
+++ src/main/java/week4/day2/ReversePyramid.java
@@ -0,0 +1,28 @@
+package week4.day2;
+      //리팩토링 1번
+    //생성자 붙이기 (생성자를 붙여야지 켜고 끌 수 있다)
+
+import java.util.Scanner;
+
+public class ReversePyramid {
+    private String spaceChar = "0";
+
+public ReversePyramid(String spaceChar) {
+        this.spaceChar = spaceChar;
+        // 역피라미드 만들기
+        //1.메소드로 분리
+        }
+    public String makeAline(int h, int i) {
+        return String.format("%s%s\n", spaceChar.repeat(i), "*".repeat(h + 4 +(-i * 2)));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ReversePyramid reversePyramid = new ReversePyramid(" ");
+        int h = sc.nextInt();
+        for (int i = 0; i < h; i++) {
+        //2.기존로직 메소드 호출 하게 변경
+            System.out.print(reversePyramid.makeAline(h,i));
+        }
+    }
+}
