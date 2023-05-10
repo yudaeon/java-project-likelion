e8860b9065e1b5099c7fff52689132b6166fb322
yudaeon <daeon03050@gmail.com>
2023-05-10 13:16:13 +0900

    feat : RightTrianglePrinter

---
+++ src/main/java/week4/day3/RightTrianglePrinter.java
@@ -0,0 +1,45 @@
+package week4.day3;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class RightTrianglePrinter {
+    //System.out --> Console
+    //Buffered Reader --
+
+    public String makeALine(int h, int i) {
+        return String.format("%s%s\n", "", "*".repeat(i));
+    }
+    //모양 출력하기
+    public void printToFile(String[] lines) throws IOException {
+        BufferedWriter bw = new BufferedWriter (new FileWriter("./aaa.txt"));
+        for (int i = 0; i < lines.length; i++) {
+            bw.append("aaa");
+            bw.flush();
+        }
+            bw.close();
+    }
+
+    public void printToConsole(String[]lines){
+        for (int i = 0; i <lines.length ; i++) {
+            System.out.print(lines[i]);
+        }
+    }
+        //모양 출력하기
+    public void printShape(int h){
+        //1. 모양 만들기
+        String[] lines = new String[h];
+        for (int i = 0; i < h; i++) {
+        lines[i] = makeALine(h, i+1);
+    }
+    // 모양 출력하기
+    printToConsole(lines);
+}
+
+    public static void main(String[] args)throws IOException {
+        RightTrianglePrinter rtp = new RightTrianglePrinter();
+        rtp.printShape(5);
+
+    }
+}
