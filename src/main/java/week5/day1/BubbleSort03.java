950b4e641dfa3c084834fefba192e15f76dccdd0
yudaeon <daeon03050@gmail.com>
2023-05-15 10:32:06 +0900

    feat : BubbleSort2

--- src/main/java/week5/day1/BubbleSort03.java
+++ src/main/java/week5/day1/BubbleSort03.java
@@ -1,10 +1,12 @@
 package week5.day1;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class BubbleSort03 {
-    public int[] sortARound(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
+
+    public int[] sortARound(int[] arr, int until) {
+        for (int i = 0; i < arr.length - 1; i++) { //1을 -->j로
             if (arr[i] > arr[i + 1]) {
                 int temp = arr[i];
                 arr[i] = arr[i + 1];
@@ -13,15 +15,17 @@ public class BubbleSort03 {
         }
         return arr;
     }
+    public int[]sort(int[]arr){
+        for (int j = 1; j <= arr.length; j++) {
+            arr = sortARound(arr, arr.length-j); //메서드 안에서만 입력하면 됨
+        }
+        return arr;
+    }
 
     public static void main(String[] args) {
         BubbleSort03 bubbleSort03 = new BubbleSort03(); //oop
         int[] arr = {7, 2, 3, 9, 28};
-
-        for (int j = 1; j <= arr.length; j++) {
-            arr = bubbleSort03.sortARound(arr);
-        }
+        arr= bubbleSort03.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
-//[2, 3, 7, 9, 28]
