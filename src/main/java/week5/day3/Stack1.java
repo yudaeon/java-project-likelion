319284e6c33440debab611271d896c7ce99ee767
yudaeon <daeon03050@gmail.com>
2023-05-17 10:18:10 +0900

    feat : Stack1(isEmpty)

--- src/main/java/week5/day3/Stack1.java
+++ src/main/java/week5/day3/Stack1.java
@@ -5,26 +5,29 @@ import java.util.Arrays;
 public class Stack1 {
     private int[]arr = new int[10000];
     private int pointer = 0;
-
-    public void  push(int value) {
+        //push
+    public void push(int value) {
         this.arr[pointer++] = value;
-//        System.out.println(Arrays.toString(arr));
-//        System.out.println("pointer = " + pointer);
+        System.out.println(Arrays.toString(arr));
+        System.out.println("pointer = " + pointer);
     }
-
-
+        //pop
     public int pop(){
-        int temp = this.arr[pointer - 1];
-        pointer--;
-        return temp;
+        return this.arr[--pointer];
+    }
+        //isEmpty
+    public boolean isEmpty(){
+        return this.pointer == 0;
     }
 
     public static void main(String[] args) {
         Stack1 st = new Stack1();
-        st.push(10);
-        st.push(20);
-        System.out.println(st.pop()); //20 LIFO
-        System.out.println(st.pop()); //10 LIFO
 
+        System.out.println(st.isEmpty()); //비었니? true
+        st.push(10);
+        System.out.println(st.isEmpty()); //비었니? false
+        st.pop();
+        System.out.println(st.isEmpty()); //false
+        st.pop();
     }
         }
