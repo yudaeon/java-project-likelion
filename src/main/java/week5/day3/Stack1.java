942adbd822e736984dc409f89d7807708ed27e89
yudaeon <daeon03050@gmail.com>
2023-05-17 10:43:16 +0900

    feat : Stack1(peek)

--- src/main/java/week5/day3/Stack1.java
+++ src/main/java/week5/day3/Stack1.java
@@ -1,33 +1,44 @@
 package week5.day3;
 
 import java.util.Arrays;
+import java.util.EmptyStackException;
 
 public class Stack1 {
     private int[]arr = new int[10000];
     private int pointer = 0;
         //push
     public void push(int value) {
-        this.arr[pointer++] = value;
-        System.out.println(Arrays.toString(arr));
-        System.out.println("pointer = " + pointer);
+        arr[pointer++] = value;
+       // System.out.println(Arrays.toString(arr));
+       // System.out.println("pointer = " + pointer);
     }
-        //pop
-    public int pop(){
-        return this.arr[--pointer];
-    }
-        //isEmpty
+    //isEmpty
     public boolean isEmpty(){
-        return this.pointer == 0;
+        return pointer == 0;
+    }
+
+    //pop
+    public int pop(){
+        if (isEmpty()) throw  new RuntimeException("스택이 비었습니다.");
+        return arr[--pointer];
+    }
+
+    //peek
+    public int peek(){
+        if (isEmpty()) throw new EmptyStackException();
+        return arr[pointer-1];
     }
 
     public static void main(String[] args) {
         Stack1 st = new Stack1();
-
-        System.out.println(st.isEmpty()); //비었니? true
         st.push(10);
-        System.out.println(st.isEmpty()); //비었니? false
-        st.pop();
-        System.out.println(st.isEmpty()); //false
-        st.pop();
+        System.out.println(st.pop()); //10
+        st.push(20);
+        System.out.println(st.peek()); //20
+
+//        System.out.println(st.isEmpty()); //비었니? true
+//        System.out.println(st.pop());
+//        System.out.println(st.isEmpty()); //비었니? false
+//        System.out.println(st.peek());
     }
         }
